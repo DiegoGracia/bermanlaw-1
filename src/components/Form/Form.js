1ff51c19bcec0fd63297d9d2b0e04c30de7a6ee5
Diego Gracia <Dgracia@dxc.com>
2022-12-05 16:18:33 -0600

    add b

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -9,7 +9,7 @@ const Form = () => {
                 <h1>gratis</h1>
             </div>
             <form className="form">
-                <input type="text" id="fname" name="Name" placeholder="Nomnre:" />
+                <input type="text" id="fname" name="Name" placeholder="Nombre:" />
                 <input type="text" id="femail" name="Email" placeholder="Correo:" />
                 <input type="number" id="fphone" name="Phone" placeholder="Teléfono:" />
                 <input type="text" id="ftellus" name="TellUs" placeholder="Cuéntanos qué sucedió:" />
