06f5399aaf8a2e71ebe8a1fa1bfaccc41439ca85
Diego Gracia <Dgracia@dxc.com>
2022-12-05 16:22:06 -0600

    lenguage changes

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -9,11 +9,11 @@ const Form = () => {
                 <h1>gratis</h1>
             </div>
             <form className="form">
-                <input type="text" id="fname" name="Name" placeholder="Name:" />
-                <input type="text" id="femail" name="Email" placeholder="Email:" />
-                <input type="number" id="fphone" name="Phone" placeholder="Phone:" />
+                <input type="text" id="fname" name="Name" placeholder="Nombre:" />
+                <input type="text" id="femail" name="Email" placeholder="Correo:" />
+                <input type="number" id="fphone" name="Phone" placeholder="Teléfono:" />
                 <input type="text" id="ftellus" name="TellUs" placeholder="Cuéntanos qué sucedió:" />
-                <input type="submit" value="Send"/>
+                <input type="submit" value="Mandar"/>
             </form>
         </div>
     );
