f34f3099c2821e2ff2a237b652fd22d88639de79
Diego Gracia <Dgracia@dxc.com>
2022-12-06 14:43:01 -0600

    added slider

--- src/components/Logos/Logos.js
+++ src/components/Logos/Logos.js
@@ -1,18 +1,12 @@
 import React from "react";
+import SliderIcons from "../SliderIcons/SliderIcons";
 import "./Logos.css"
-import { LogoData } from "../../data/LogosList"
 
 const Logos = () => {
     return (
-        <div className="logos-container">
-            {LogoData.map((item, index) => {
-                return (
-                    <>
-                        <img src={item.logo} alt="test"/>
-                    </>
-                );
-            })}
-        </div>
+        <>
+            <SliderIcons/>
+        </>
     );
 };
 
