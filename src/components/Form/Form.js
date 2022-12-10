6a24213be88c760135a49a528b0d54941f5da030
Diego Gracia <Dgracia@dxc.com>
2022-12-10 06:38:09 -0600

    form its working

--- src/components/Form/Form.js
+++ src/components/Form/Form.js
@@ -1,18 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Form.css"
 
 const Form = () => {
+    const [formState, setFormState] = useState({});
+
+    const changeHandler = (event) => {
+        setFormState({ ...formState, [event.target.name]: event.target.value })
+    }
+
+    const submitHandler = (event) => {
+        event.preventDefault()
+        const config = {
+            SecureToken: "36c56350-fe05-4e9b-8ba0-4b861e4bd416",
+            To : 'MMuscarella@thebermanlawgroup.com',
+            From : "service@10xlawflorida.com",
+            Subject : "contact",
+            Body : `this is the info: ${formState.Name},${formState.Email}, ${formState.Number}, ${formState.TellUs}`
+        }
+        const config2 = {
+            SecureToken: "36c56350-fe05-4e9b-8ba0-4b861e4bd416",
+            To : 'RBerman@thebermanlawgroup.com',
+            From : "service@10xlawflorida.com",
+            Subject : "contact",
+            Body : `this is the info: ${formState.Name},${formState.Email}, ${formState.Number}, ${formState.TellUs}`
+        }
+        if(window.Email){
+            window.Email.send(config).then(() => alert("correo enviado de manera exitosa"))
+            window.Email.send(config2)
+        }
+    }
     return(
         <div className="form-container">
             <div className="form-title-container">
                 <h1>evaluación</h1>
                 <h1>gratis</h1>
             </div>
-            <form className="form">
-                <input type="text" id="fname" name="Name" placeholder="Nombre:" />
-                <input type="text" id="femail" name="Email" placeholder="Correo:" />
-                <input type="number" id="fphone" name="Phone" placeholder="Teléfono:" />
-                <input type="text" id="ftellus" name="TellUs" placeholder="Cuéntanos qué sucedió:" />
+            <form className="form" onSubmit={submitHandler}>
+                <input type="text" name="Name" placeholder="Nombre:" onChange={changeHandler} />
+                <input type="email" name="Email" placeholder="Correo:" onChange={changeHandler}/>
+                <input type="number" name="Phone" placeholder="Teléfono:"  onChange={changeHandler}/>
+                <input type="text" name="TellUs" placeholder="Cuéntanos qué sucedió:" onChange={changeHandler} />
                 <input type="submit" value="Enviar"/>
             </form>
         </div>
